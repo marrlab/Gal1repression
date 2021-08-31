ff40e5eaab0eefd09c2d33da1da29471d10d2a3a
Lea Schuh <lea.schuh@tum.de>
2021-08-31 17:58:21 +0200

    Update plotFigure4.m

--- plotFigure4.m
+++ plotFigure4.m
@@ -1,6 +1,159 @@
 %add path
 addpath(genpath(pwd))
 
+%% Figure 4A/E and B/F - non-dividing total GFP traces for repressions 1 and 2 for WT and elp6 wihtout non-repressors
+
+clearvars;
+clc;
+
+load('NonDividing')
+count = 1;
+
+for strain1 = 1:2
+    for rep1 = 1:2
+        
+        figure('visible','off');
+        
+        clear ind1_2 BIC1_1 BIC1_2
+        
+        %istrain = 1 - WT / = 2 - elp6
+        %irep = 1 - repression 1 / = 2 - repression 2
+        
+        %load all estimated parameter sets for both models and repressions
+        load(sprintf('scR_strain%d_rep%d_model%d',strain1,rep1,1))
+        scR1_1 = scR;
+        load(sprintf('scR_strain%d_rep%d_model%d',strain1,rep1,2))
+        scR1_2 = scR;
+        
+        %extract BIC values for all single-cell trajectories for data set 1
+        for i = 1:size(scR1_1,2)
+            BIC1_1(i) = scR1_1(i).sol.BIC;
+        end
+        for i = 1:size(scR1_2,2)
+            BIC1_2(i) = scR1_2(i).sol.BIC;
+        end
+        
+        %decide whether single-cell requires repressor model or not for data set 1
+        ind1_2 = find(BIC1_2-BIC1_1<-10); %model 2 best
+        ind1_1 = find(BIC1_2-BIC1_1>=-10);%model 1 best
+        
+        istrain = strain1;
+        irep = rep1;
+        
+        %define color according to strain and repression
+        if istrain == 1
+            if irep == 1
+                c = [117,157,233]./255;
+            else
+                c = [33,68,120]./255;
+            end
+        else
+            if irep == 1
+                c = [205,135,222]./255;
+            else
+                c = [67,31,117]./255;
+            end
+        end
+        
+        %plot the non-dividing cells of specified strain and repression
+        %also plot mean total GFP trace and maximal mean total GFP value
+        if irep == 1
+            plot((1-1)*3/60:3/60:(40-1)*3/60,NonDividing{istrain}.I5r1(ind1_2,1:40),'-','Color',c)
+            hold on
+            plot((1-1)*3/60:3/60:(40-1)*3/60,mean(NonDividing{istrain}.I5r1(ind1_2,1:40)),'-','Color','k')
+            hold on
+            indmax = find(mean(NonDividing{istrain}.I5r1(ind1_2,1:40))==max(mean(NonDividing{istrain}.I5r1(ind1_2,1:40))));
+            time = (1-1)*3/60:3/60:(40-1)*3/60;
+            time(indmax)
+            plot(time(indmax),max(mean(NonDividing{istrain}.I5r1(ind1_2,1:40))),'.','Color','k','Markersize',12)
+            
+            for isample = 1:100000
+                S = datasample(1:length(ind1_2),round(length(ind1_2)*0.25));
+                indmax = find(mean(NonDividing{istrain}.I5r1(ind1_2(S),1:40))==max(mean(NonDividing{istrain}.I5r1(ind1_2(S),1:40))));
+                T(count,isample) = time(indmax);
+            end
+            %
+            
+        else
+            plot((1-1)*3/60:3/60:(40-1)*3/60,NonDividing{istrain}.I5r2(ind1_2,1:40),'-','Color',c)
+            hold on
+            plot((1-1)*3/60:3/60:(40-1)*3/60,mean(NonDividing{istrain}.I5r2(ind1_2,1:40)),'-','Color','k')
+            hold on
+            indmax = find(mean(NonDividing{istrain}.I5r2(ind1_2,1:40))==max(mean(NonDividing{istrain}.I5r2(ind1_2,1:40))));
+            time = (1-1)*3/60:3/60:(40-1)*3/60;
+            time(indmax)
+            plot(time(indmax),max(mean(NonDividing{istrain}.I5r2(ind1_2,1:40))),'.','Color','k','Markersize',12)
+            
+            for isample = 1:100000
+                S = datasample(1:length(ind1_2),round(length(ind1_2)*0.25));
+                indmax = find(mean(NonDividing{istrain}.I5r2(ind1_2(S),1:40))==max(mean(NonDividing{istrain}.I5r2(ind1_2(S),1:40))));
+                T(count,isample) = time(indmax);
+            end
+            %
+            
+        end
+        
+        count = count+1;
+        
+        ylabel('GFP intensity')
+        if irep == 1
+            yticks([0,2e7,4e7])
+            ylim([0,5e7])
+        else
+            yticks([0,5e7,10e7,15e7])
+            ylim([0,15e7])
+        end
+        xlabel('time (h)')
+        xticks([0,1,2])
+        box off
+        set(gca,'linewidth',1.02)
+        set(gca,'FontSize',11)
+        set(gca,'FontName','Arial')
+        xlim([0,2])
+        set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 5.5 5])
+        if istrain == 1
+            if irep == 1
+                print('-dpdf','./Figures/Fig4Aleft','-painters')
+            else
+                print('-dpdf','./Figures/Fig4Aright','-painters')
+            end
+        else
+            if irep == 1
+                print('-dpdf','./Figures/Fig4Eleft','-painters')
+            else
+                print('-dpdf','./Figures/Fig4Eright','-painters')
+            end
+        end
+    end
+end
+
+figure
+for icount = 1:size(T,1)
+    
+    line([mean(T(icount,:)),mean(T(icount,:))],[icount-0.4,icount+0.4],'Color','k','Linewidth',1)
+    hold on
+    line([mean(T(icount,:))-std(T(icount,:)),mean(T(icount,:))+std(T(icount,:))],[icount,icount],'Color','k','Linewidth',1)
+    hold on
+    line([mean(T(icount,:))+std(T(icount,:)),mean(T(icount,:))+std(T(icount,:))],[icount-0.2,icount+0.2],'Color','k','Linewidth',1)
+    hold on
+    line([mean(T(icount,:))-std(T(icount,:)),mean(T(icount,:))-std(T(icount,:))],[icount-0.2,icount+0.2],'Color','k','Linewidth',1)
+    
+end
+
+% xlim([0,5])
+set(gca,'FontSize',10)
+xlim([0, 2])
+box off
+set(gca,'linewidth',1.02)
+set(gca,'FontSize',11)
+set(gca,'FontName','Arial')
+xlabel('data set')
+ylabel('maximal mean total GFP')
+
+%save figure
+set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 5.5 5])
+print('-dpdf','./Figures/Fig4BF','-painters')
+
 %% Figure 4A-C - estimated parameter comparison (WT/elp6)
 
 %strain = 1 - WT / = 2 - elp6
@@ -182,7 +335,7 @@ end
 %save figure
 set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 13 4])
 if strain1 == 1
-   print('-dpdf','./Figures/Fig4A','-painters')
+    print('-dpdf','./Figures/Fig4A','-painters')
 else
     print('-dpdf','./Figures/Fig4C','-painters')
 end
@@ -276,7 +429,7 @@ end
 Par1 = Par1(ind1_2,:);
 Par2 = Par2(ind2_2,:);
 
-%determine color according ot strain and repression 
+%determine color according ot strain and repression
 if strain1 == 1
     if rep1 == 1
         c1 = [175,198,233]./255;
@@ -307,7 +460,7 @@ end
 
 figure('visible','off');
 
-%plot GFP0 vs tdelay for both data sets 
+%plot GFP0 vs tdelay for both data sets
 plot(Par2(:,1),Par2(:,2),'.','Color',c2,'Markersize',10)
 hold on
 plot(Par1(:,1),Par1(:,2),'.','Color',c1,'Markersize',10)
@@ -350,7 +503,7 @@ end
 %save figure
 set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 5 5])
 if strain1 == 1
-   print('-dpdf','./Figures/Fig4B','-painters')
+    print('-dpdf','./Figures/Fig4B','-painters')
 else
     print('-dpdf','./Figures/Fig4D','-painters')
 end
