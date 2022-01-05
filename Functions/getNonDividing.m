0c976dc2a276aa74f24cf0308860292d93c74a3e
Lea Schuh <lea.schuh@tum.de>
2022-01-05 14:44:37 +0100

    Update getNonDividing.m

--- Functions/getNonDividing.m
+++ Functions/getNonDividing.m
@@ -5,7 +5,7 @@ clc;
 
 for istrain = 1:2
     
-    clearvars -except istrain NonDividing fig_count
+    clearvars -except istrain NonDividing fig_count iexp
     
     %get correct data sets (Wt vs elp6)
     if istrain == 1
@@ -16,60 +16,69 @@ for istrain = 1:2
             DataExp{2} = sprintf('ExpI5_pos2_Y208_WT');
             DataExp{3} = sprintf('ExpI5_pos4_Y208_WT');
             DataExp{4} = sprintf('ExpI5_pos5_Y208_WT');
-            DataExp{6} = sprintf('ExpI5_pos8_Y208_WT');
-            DataExp{7} = sprintf('ExpI5_pos9_Y208_WT');
-            DataExp{8} = sprintf('ExpI5_pos10_Y208_WT');
-            DataExp{9} = sprintf('ExpI5_pos11_Y208_WT');
-            DataExp{10} = sprintf('ExpI5_pos12_Y208_WT');
-            DataExp{11} = sprintf('ExpI5_pos13_Y208_WT');
-            DataExp{12} = sprintf('ExpI5_pos14_Y208_WT');
-            DataExp{13} = sprintf('ExpI5_pos15_Y208_WT');
-            DataExp{14} = sprintf('ExpI5_pos16_Y208_WT');
+            DataExp{5} = sprintf('ExpI5_pos8_Y208_WT');
+            DataExp{6} = sprintf('ExpI5_pos9_Y208_WT');
+            DataExp{7} = sprintf('ExpI5_pos10_Y208_WT');
+            DataExp{8} = sprintf('ExpI5_pos11_Y208_WT');
+            DataExp{9} = sprintf('ExpI5_pos12_Y208_WT');
+            DataExp{10} = sprintf('ExpI5_pos13_Y208_WT');
+            DataExp{11} = sprintf('ExpI5_pos14_Y208_WT');
+            DataExp{12} = sprintf('ExpI5_pos15_Y208_WT');
+            DataExp{13} = sprintf('ExpI5_pos16_Y208_WT');
         else
-            % DataExp{1} = sprintf('ExpL1_pos1_Y208_WT');
-            % DataExp{2} = sprintf('ExpL1_pos2_Y208_WT');
-            % DataExp{3} = sprintf('ExpL1_pos6_Y208_WT');
+            DataExp{1} = sprintf('ExpL1_pos1_Y208_WT');
+            DataExp{2} = sprintf('ExpL1_pos5_Y208_WT');
+            DataExp{3} = sprintf('ExpL1_pos6_Y208_WT');
+            DataExp{4} = sprintf('ExpL1_pos7_Y208_WT');
+            DataExp{5} = sprintf('ExpL1_pos8_Y208_WT');
+            DataExp{6} = sprintf('ExpL2_pos1_Y208_WT');
+            DataExp{7} = sprintf('ExpL2_pos4_Y208_WT');
+            DataExp{8} = sprintf('ExpL2_pos6_Y208_WT');
+            DataExp{9} = sprintf('ExpL2_pos9_Y208_WT');
+            DataExp{10} = sprintf('ExpL2_pos10_Y208_WT');
+            DataExp{11} = sprintf('ExpL2_pos11_Y208_WT');
+            DataExp{12} = sprintf('ExpL2_pos12_Y208_WT');
+            DataExp{13} = sprintf('ExpI7_pos4_Y208_WT');
         end
         
     else
-        clear Data
         
+        clear Data
         if iexp == 1
-            DataExp{15} = sprintf('ExpI5_pos19_Y1474_Elp6');
-            DataExp{16} = sprintf('ExpI5_pos20_Y1474_Elp6');
-            DataExp{17} = sprintf('ExpI5_pos22_Y1474_Elp6');
-            DataExp{18} = sprintf('ExpI5_pos23_Y1474_Elp6');
-            DataExp{19} = sprintf('ExpI5_pos24_Y1474_Elp6');
-            DataExp{20} = sprintf('ExpI5_pos25_Y1474_Elp6');
-            DataExp{21} = sprintf('ExpI5_pos27_Y1474_Elp6');
-            DataExp{22} = sprintf('ExpI5_pos30_Y1474_Elp6');
-            DataExp{23} = sprintf('ExpI5_pos31_Y1474_Elp6');
-            DataExp{24} = sprintf('ExpI5_pos32_Y1474_Elp6');
+            DataExp{1} = sprintf('ExpI5_pos19_Y1474_Elp6');
+            DataExp{2} = sprintf('ExpI5_pos20_Y1474_Elp6');
+            DataExp{3} = sprintf('ExpI5_pos22_Y1474_Elp6');
+            DataExp{4} = sprintf('ExpI5_pos23_Y1474_Elp6');
+            DataExp{5} = sprintf('ExpI5_pos24_Y1474_Elp6');
+            DataExp{6} = sprintf('ExpI5_pos25_Y1474_Elp6');
+            DataExp{7} = sprintf('ExpI5_pos27_Y1474_Elp6');
+            DataExp{8} = sprintf('ExpI5_pos30_Y1474_Elp6');
+            DataExp{9} = sprintf('ExpI5_pos31_Y1474_Elp6');
+            DataExp{10} = sprintf('ExpI5_pos32_Y1474_Elp6');
         else
-            DataExp{25} = sprintf('ExpL1_pos9_Y1474_Elp6');
-            DataExp{26} = sprintf('ExpL1_pos10_Y1474_Elp6');
-            DataExp{27} = sprintf('ExpL1_pos11_Y1474_Elp6');
-            DataExp{28} = sprintf('ExpL1_pos12_Y1474_Elp6');
-            DataExp{29} = sprintf('ExpL1_pos13_Y1474_Elp6');
-            DataExp{30} = sprintf('ExpL1_pos14_Y1474_Elp6');
-            DataExp{31} = sprintf('ExpL1_pos15_Y1474_Elp6');
-            DataExp{32} = sprintf('ExpL1_pos16_Y1474_Elp6');
-            
-            DataExp{33} = sprintf('ExpL2_pos14_Y1474_Elp6');
-            DataExp{34} = sprintf('ExpL2_pos15_Y1474_Elp6');
-            DataExp{35} = sprintf('ExpL2_pos16_Y1474_Elp6');
-            DataExp{36} = sprintf('ExpL2_pos17_Y1474_Elp6');
-            DataExp{37} = sprintf('ExpL2_pos18_Y1474_Elp6');
-            DataExp{38} = sprintf('ExpL2_pos19_Y1474_Elp6');
-            DataExp{39} = sprintf('ExpL2_pos20_Y1474_Elp6');
-            DataExp{40} = sprintf('ExpL2_pos21_Y1474_Elp6');
-            DataExp{41} = sprintf('ExpL2_pos22_Y1474_Elp6');
-            DataExp{42} = sprintf('ExpL2_pos23_Y1474_Elp6');
-            DataExp{43} = sprintf('ExpL2_pos24_Y1474_Elp6');
-            DataExp{44} = sprintf('ExpL2_pos25_Y1474_Elp6');
-            DataExp{45} = sprintf('ExpL2_pos26_Y1474_Elp6');
-            DataExp{46} = sprintf('ExpL2_pos27_Y1474_Elp6');
-            DataExp{47} = sprintf('ExpL2_pos28_Y1474_Elp6');
+            DataExp{1} = sprintf('ExpL1_pos9_Y1474_Elp6');
+            DataExp{2} = sprintf('ExpL1_pos10_Y1474_Elp6');
+            DataExp{3} = sprintf('ExpL1_pos11_Y1474_Elp6');
+            DataExp{4} = sprintf('ExpL1_pos12_Y1474_Elp6');
+            DataExp{5} = sprintf('ExpL1_pos13_Y1474_Elp6');
+            DataExp{6} = sprintf('ExpL1_pos14_Y1474_Elp6');
+            DataExp{7} = sprintf('ExpL1_pos15_Y1474_Elp6');
+            DataExp{8} = sprintf('ExpL1_pos16_Y1474_Elp6');
+            DataExp{9} = sprintf('ExpL2_pos14_Y1474_Elp6');
+            DataExp{10} = sprintf('ExpL2_pos15_Y1474_Elp6');
+            DataExp{11} = sprintf('ExpL2_pos16_Y1474_Elp6');
+            DataExp{12} = sprintf('ExpL2_pos17_Y1474_Elp6');
+            DataExp{13} = sprintf('ExpL2_pos18_Y1474_Elp6');
+            DataExp{14} = sprintf('ExpL2_pos19_Y1474_Elp6');
+            DataExp{15} = sprintf('ExpL2_pos20_Y1474_Elp6');
+            DataExp{16} = sprintf('ExpL2_pos21_Y1474_Elp6');
+            DataExp{17} = sprintf('ExpL2_pos22_Y1474_Elp6');
+            DataExp{18} = sprintf('ExpL2_pos23_Y1474_Elp6');
+            DataExp{19} = sprintf('ExpL2_pos24_Y1474_Elp6');
+            DataExp{20} = sprintf('ExpL2_pos25_Y1474_Elp6');
+            DataExp{21} = sprintf('ExpL2_pos26_Y1474_Elp6');
+            DataExp{22} = sprintf('ExpL2_pos27_Y1474_Elp6');
+            DataExp{23} = sprintf('ExpL2_pos28_Y1474_Elp6');
         end
     end
     
@@ -102,13 +111,16 @@ for istrain = 1:2
             mom_posr1all mom_posr2all daughters_IDr1all mother_count_r1all...
             daughters_IDr2all mother_count_r2all iexp
         
-        loadData = sprintf('./Data/S%s',Data{i});
+        loadData = sprintf('S%s',Data{i});
         load(loadData);
         
         count_mother_r1 = 1;
         count_mother_r2 = 1;
         count_daughter_r1 = 1;
         count_daughter_r2 = 1;
+        
+        daughters_IDr1 = [];
+        daughters_IDr2 = [];
         
         clear cell_info
         
@@ -214,7 +226,7 @@ for istrain = 1:2
                 
                 %save total GFP of mother cell for frames 281:last frame
                 %(first two hours of repression 2)
-                momGFPr2(count_mother_r2,:) = S{iS}.GFPabs(f281+1:end);
+                momGFPr2(count_mother_r2,:) = S{iS}.GFPabs(f281+1:f281+40);
                 
                 %save mother ID and position and initialize daughter
                 %count to 0 for this mother cell
@@ -253,8 +265,8 @@ for istrain = 1:2
                         %add daughter GFP to mother GFP for the
                         %overlapping time frames between mother and
                         %daughter for the first two hours of repression 2
-                        momGFPr2(ind,S{iS}.DF-280:S{iS}.LF-280) = ...
-                            momGFPr2(ind,S{iS}.DF-280:S{iS}.LF-280)+S{iS}.GFPabs;
+                        momGFPr2(ind,S{iS}.DF-280:320-280) = ...
+                            momGFPr2(ind,S{iS}.DF-280:320-280)+S{iS}.GFPabs(1:320-S{iS}.DF+1);
                         
                         %save daughter ID and update the count of
                         %daughter cells for this particular mother cell
@@ -290,31 +302,31 @@ for istrain = 1:2
     momGFPr2all(~any(momGFPr2all,2),:) = [];
     
     %save everything in MATLAB structure (for experiment I5)
-    NonDividing{istrain}.I5r1 = momGFPr1all;
-    NonDividing{istrain}.I5r2 = momGFPr2all;
-    NonDividing{istrain}.I5nr1 =  n_cells_r1;
-    NonDividing{istrain}.I5nr2 =  n_cells_r2;
-    NonDividing{istrain}.I5momIDr1 =  mom_IDr1all;
-    NonDividing{istrain}.I5momIDr2 =  mom_IDr2all;
-    NonDividing{istrain}.I5momposr1 =  mom_posr1all;
-    NonDividing{istrain}.I5momposr2 =  mom_posr2all;
-    NonDividing{istrain}.I5momcountr1 = mother_count_r1all;
-    NonDividing{istrain}.I5momcountr2 = mother_count_r2all;
-    NonDividing{istrain}.I5daughtersIDr1 = daughters_IDr1all;
-    NonDividing{istrain}.I5daughtersIDr2 = daughters_IDr2all;
+    NonDividing{istrain}.r1 = momGFPr1all;
+    NonDividing{istrain}.r2 = momGFPr2all;
+    NonDividing{istrain}.nr1 =  n_cells_r1;
+    NonDividing{istrain}.nr2 =  n_cells_r2;
+    NonDividing{istrain}.momIDr1 =  mom_IDr1all;
+    NonDividing{istrain}.momIDr2 =  mom_IDr2all;
+    NonDividing{istrain}.momposr1 =  mom_posr1all;
+    NonDividing{istrain}.momposr2 =  mom_posr2all;
+    NonDividing{istrain}.momcountr1 = mother_count_r1all;
+    NonDividing{istrain}.momcountr2 = mother_count_r2all;
+    NonDividing{istrain}.daughtersIDr1 = daughters_IDr1all;
+    NonDividing{istrain}.daughtersIDr2 = daughters_IDr2all;
 end
 
 if iexp == 1
-    save('./Data/NonDividing','NonDividing')
-    xlswrite('./Data/NonDividing_WT_rep1.xlsx',NonDividing{1}.I5r1,1,'A1');
-    xlswrite('./Data/NonDividing_WT_rep2.xlsx',NonDividing{1}.I5r2,1,'A1');    
-    xlswrite('./Data/NonDividing_elp6_rep1.xlsx',NonDividing{2}.I5r1,1,'A1');
-    xlswrite('./Data/NonDividing_elp6_rep2.xlsx',NonDividing{2}.I5r2,1,'A1');
+    save('./Melanie/Data/NonDividing1','NonDividing')
+    %     xlswrite('./Data/NonDividing1_WT_rep1.xlsx',NonDividing{1}.I5r1,1,'A1');
+    %     xlswrite('./Data/NonDividing1_WT_rep2.xlsx',NonDividing{1}.I5r2,1,'A1');
+    %     xlswrite('./Data/NonDividing1_elp6_rep1.xlsx',NonDividing{2}.I5r1,1,'A1');
+    %     xlswrite('./Data/NonDividing1_elp6_rep2.xlsx',NonDividing{2}.I5r2,1,'A1');
 else
-    save('./Data/NonDividing2','NonDividing')  
-%     xlswrite('./Data/NonDividing2_WT_rep1.xlsx',NonDividing{1}.I5r1,1,'A1');
-%     xlswrite('./Data/NonDividing2_WT_rep2.xlsx',NonDividing{1}.I5r2,1,'A1');
-    xlswrite('./Data/NonDividing2_elp6_rep1.xlsx',NonDividing{2}.I5r1,1,'A1');
-    xlswrite('./Data/NonDividing2_elp6_rep2.xlsx',NonDividing{2}.I5r2,1,'A1');
+    save('./Melanie/Data/NonDividing2','NonDividing')
+    %     xlswrite('./Data/NonDividing2_WT_rep1.xlsx',NonDividing{1}.I5r1,1,'A1');
+    %     xlswrite('./Data/NonDividing2_WT_rep2.xlsx',NonDividing{1}.I5r2,1,'A1');
+    %     xlswrite('./Data/NonDividing2_elp6_rep1.xlsx',NonDividing{2}.I5r1,1,'A1');
+    %     xlswrite('./Data/NonDividing2_elp6_rep2.xlsx',NonDividing{2}.I5r2,1,'A1');
 end
 
