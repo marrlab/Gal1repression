b5afbfe0450b4b3c8b5a97709e4b615fb48a5dd6
Lea Schuh <lea.schuh@tum.de>
2022-02-10 11:07:19 +0100

    Update plotFigureS1.m

--- plotFigureS1.m
+++ plotFigureS1.m
@@ -92,9 +92,9 @@ for i = 1:length(Data)
     
 end
 
-ylabel('GFP intensity')
+ylabel('total Gal1-GFP fluorescence (a.u.)')
 yticks([0,2,4])
-xlabel('time (h)')
+xlabel('experimental time (h)')
 xticks([0,4,7,11,14,16])
 box off
 set(gca,'linewidth',1.02)
@@ -193,7 +193,7 @@ for irep = 1:2
         
     end
     
-    ylabel('GFP intensity')
+    ylabel('total GFP (a.u.)')
     if irep == 1
         yticks([0,1,2])
         ylim([0,2])
@@ -201,7 +201,7 @@ for irep = 1:2
         yticks([0,2,4])
         ylim([0,4])
     end
-    xlabel('time (h)')
+    xlabel('repression time (h)')
     xticks([0,1,2])
     box off
     set(gca,'linewidth',1.02)
@@ -221,6 +221,8 @@ end
 %istrain = 1 - WT / = 2 - elp6
 %irep = 1 - repression 1 / = 2 - repression 2
 
+rand('seed', 6);
+
 istrain = 1;
 for  irep = 1:2
     
@@ -229,8 +231,8 @@ for  irep = 1:2
     
     %load data of computed non-dividing cells
     load('NonDividing2')
-    figure('visible','off');
-    %         figure
+%     figure('visible','off');
+            figure
     
     %define color according to strain and repression
     if irep == 1
@@ -333,8 +335,8 @@ for  irep = 1:2
         end
     end
     
-    ylabel('GFP intensity')
-    xlabel('time (h)')
+    ylabel('total GFP (a.u.)')
+    xlabel('repression time (h)')
     xticks([0,1,2])
     box off
     set(gca,'linewidth',1.02)
@@ -438,8 +440,7 @@ for irep = 1:2
         set(gca,'linewidth',1.02)
         set(gca,'FontSize',11)
         set(gca,'FontName','Arial')
-        ylabel('GFP intensity')
-        xlabel('model')
+        ylabel('GFP_0')
     end
     
     %save figure
@@ -520,23 +521,24 @@ for strain1 = 1
     end
 end
 
-% figure('visible','off');
-figure
+figure('visible','off');
 for icount = 1:size(T,1)
     
     if icount == 1
         c = [117,157,233]./255;
+        y = 2;
     else
         c = [33,68,120]./255;
+        y = 1;
     end
     
-    line([mean(T(icount,:)),mean(T(icount,:))],[icount-0.4,icount+0.4],'Color',c,'Linewidth',1)
+    line([mean(T(icount,:)),mean(T(icount,:))],[y-0.4,y+0.4],'Color',c,'Linewidth',1)
     hold on
-    line([mean(T(icount,:))-std(T(icount,:)),mean(T(icount,:))+std(T(icount,:))],[icount,icount],'Color',c,'Linewidth',1)
+    line([mean(T(icount,:))-std(T(icount,:)),mean(T(icount,:))+std(T(icount,:))],[y,y],'Color',c,'Linewidth',1)
     hold on
-    line([mean(T(icount,:))+std(T(icount,:)),mean(T(icount,:))+std(T(icount,:))],[icount-0.2,icount+0.2],'Color',c,'Linewidth',1)
+    line([mean(T(icount,:))+std(T(icount,:)),mean(T(icount,:))+std(T(icount,:))],[y-0.2,y+0.2],'Color',c,'Linewidth',1)
     hold on
-    line([mean(T(icount,:))-std(T(icount,:)),mean(T(icount,:))-std(T(icount,:))],[icount-0.2,icount+0.2],'Color',c,'Linewidth',1)
+    line([mean(T(icount,:))-std(T(icount,:)),mean(T(icount,:))-std(T(icount,:))],[y-0.2,y+0.2],'Color',c,'Linewidth',1)
     
 end
 
@@ -547,6 +549,8 @@ box off
 set(gca,'linewidth',1.02)
 set(gca,'FontSize',11)
 set(gca,'FontName','Arial')
+xlabel('time to maximal mean total GFP (h)')
+ylim([0,3])
 
 %save figure
 set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 5.5 5])
@@ -747,128 +751,3 @@ end
 %save figure
 set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 13 4])
 print('-dpdf','./Figures/FigS1F','-painters')
-
-%% Figure S1G - GFP0 vs tdelay for WT - replicate
-
-%strain = 1 - WT / = 2 - elp6
-%rep = 1 - repression 1 / = 2 - repression 2
-
-strain1 = 1;
-rep1 = 1;
-strain2 = 1;
-rep2 = 2;
-
-clearvars -except rep1 strain1 rep2 strain2 paired
-clc;
-
-%load all estimated parameter sets for both models and repressions
-load(sprintf('scR2_strain%d_rep%d_model%d',strain1,rep1,1))
-scR1_1 = scR;
-load(sprintf('scR2_strain%d_rep%d_model%d',strain1,rep1,2))
-scR1_2 = scR;
-
-load(sprintf('scR2_strain%d_rep%d_model%d',strain2,rep2,1))
-scR2_1 = scR;
-load(sprintf('scR2_strain%d_rep%d_model%d',strain2,rep2,2))
-scR2_2 = scR;
-
-%extract BIC values for all single-cell trajectories for data set 1
-for i = 1:size(scR1_1,2)
-    BIC1_1(i) = scR1_1(i).sol.BIC;
-end
-for i = 1:size(scR1_2,2)
-    BIC1_2(i) = scR1_2(i).sol.BIC;
-end
-
-%decide whether single-cell requires repressor model or not for data set 1
-ind1_2 = find(BIC1_2-BIC1_1<-10); %model 2 best
-ind1_1 = find(BIC1_2-BIC1_1>=-10);%model 1 best
-
-%extract BIC values for all single-cell trajectories for data set 2
-for i = 1:size(scR2_1,2)
-    BIC2_1(i) = scR2_1(i).sol.BIC;
-end
-for i = 1:size(scR2_2,2)
-    BIC2_2(i) = scR2_2(i).sol.BIC;
-end
-
-%decide whether single-cell requires repressor model or not for data set 2
-ind2_2 = find(BIC2_2-BIC2_1<-10);
-ind2_1 = find(BIC2_2-BIC2_1>=-10);
-
-%extract the estimated parameter sets per cell and data set
-for icell = 1:length(scR1_2)
-    clear par
-    par = 10.^(scR1_2(icell).sol.MS.par(:,1));
-    Par1(icell,:) = par';
-end
-
-for icell = 1:length(scR2_2)
-    clear par
-    par = 10.^(scR2_2(icell).sol.MS.par(:,1));
-    Par2(icell,:) = par';
-end
-
-%reduce parameter sets to the cells requiring the repressor model
-Par1 = Par1(ind1_2,:);
-Par2 = Par2(ind2_2,:);
-
-%determine color according ot strain and repression
-if rep1 == 1
-    c1 = [175,198,233]./255;
-else
-    c1 = [33,68,120]./255;
-end
-
-if rep2 == 1
-    c2 = [175,198,233]./255;
-else
-    c2 = [33,68,120]./255;
-end
-
-% figure('visible','off');
-figure
-
-%plot GFP0 vs tdelay for both data sets
-plot(Par2(:,1),Par2(:,2),'.','Color',c2,'Markersize',10)
-hold on
-plot(Par1(:,1),Par1(:,2),'.','Color',c1,'Markersize',10)
-hold on
-
-%get linear regression fit
-con1 = polyfit(Par1(:,1),Par1(:,2),1);
-% Display evaluated equation y = m*x + b
-disp(['Repression 1: Equation is y = ' num2str(con1(1)) '*x + ' num2str(con1(2))]);
-% Evaluate fit equation using polyval
-y1_est = polyval(con1,Par1(:,1));
-% Add trend line to plot
-hold on
-plot(Par1(:,1),y1_est,'-','Color',c1,'LineWidth',2)
-hold on
-
-con2 = polyfit(Par2(:,1),Par2(:,2),1);
-% Display evaluated equation y = m*x + b
-disp(['Repression 2: Equation is y = ' num2str(con2(1)) '*x + ' num2str(con2(2))]);
-% Evaluate fit equation using polyval
-y2_est = polyval(con2,Par2(:,1));
-% Add trend line to plot
-hold on
-plot(Par2(:,1),y2_est,'-','Color',c2,'LineWidth',2)
-
-ylim([0 inf])
-box off
-set(gca,'linewidth',1.02)
-set(gca,'FontSize',11)
-set(gca,'FontName','Arial')
-xlabel('GFP0')
-ylabel('tdelay')
-if strain1 == 1
-    xlim([0,4])
-    xticks([0,2,4])
-    yticks([0,1])
-end
-
-%save figure
-% set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 5 5])
-% print('-dpdf','./Figures/FigS1G','-painters')
-
