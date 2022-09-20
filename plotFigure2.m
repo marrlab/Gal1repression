aced9aded8b6de00a00dccc7d396e5cd4a5f5f5c
leaschuh <lea.schuh@tum.de>
2022-09-20 13:36:43 +0200

    Update plotFigure2.m

--- plotFigure2.m
+++ plotFigure2.m
@@ -1,110 +1,7 @@
 %add path
 addpath(genpath(pwd))
 
-%% Figure 2B - full data set WT
-clearvars;
-clc;
-
-figure('visible','off');
-clearvars
-
-%WT
-DataExpI5{1} = sprintf('ExpI5_pos1_Y208_WT');
-DataExpI5{2} = sprintf('ExpI5_pos2_Y208_WT');
-DataExpI5{3} = sprintf('ExpI5_pos4_Y208_WT');
-DataExpI5{4} = sprintf('ExpI5_pos5_Y208_WT');
-DataExpI5{5} = sprintf('ExpI5_pos8_Y208_WT');
-DataExpI5{6} = sprintf('ExpI5_pos9_Y208_WT');
-DataExpI5{7} = sprintf('ExpI5_pos10_Y208_WT');
-DataExpI5{8} = sprintf('ExpI5_pos11_Y208_WT');
-DataExpI5{9} = sprintf('ExpI5_pos12_Y208_WT');
-DataExpI5{10} = sprintf('ExpI5_pos13_Y208_WT');
-DataExpI5{11} = sprintf('ExpI5_pos14_Y208_WT');
-DataExpI5{12} = sprintf('ExpI5_pos15_Y208_WT');
-DataExpI5{13} = sprintf('ExpI5_pos16_Y208_WT');
-
-Data = DataExpI5;
-
-Data = Data(~cellfun('isempty',Data));
-count = 0;
-
-for i = 1:length(Data)
-    
-    clearvars -except Data i istrain
-    
-    %load different positions
-    loadData = sprintf('S%s',Data{i});
-    load(loadData);
-    
-    %for every cell in that position do
-    for iS = 1:length(S)
-        
-        %color definition
-        c = [175,198,233;160,160,160;117,157,233;160,160,160;33,68,120]./255;
-        
-        %repression 0
-        ind81 = find(S{iS}.DF:S{iS}.LF==81);
-        if isempty(ind81) == 0
-            plot((81-length(S{iS}.GFPabs(1:ind81)))*3/60:3/60:(81-1)*3/60,...
-                S{iS}.GFPabs(1:ind81)./10e6,'-','Color',c(1,:))
-            hold on
-        else
-            ind81 = 1;
-        end
-        
-        %induction 1
-        ind141 = find(S{iS}.DF:S{iS}.LF==141);
-        if isempty(ind141) == 0
-            plot((141-length(S{iS}.GFPabs(ind81:ind141)))*3/60:3/60:(141-1)*3/60,...
-                S{iS}.GFPabs(ind81:ind141)./10e6,'-','Color',c(2,:))
-            hold on
-        else
-            ind141 = 1;
-        end
-        
-        %repression 1
-        ind221 = find(S{iS}.DF:S{iS}.LF==221);
-        if isempty(ind221) == 0
-            plot((221-length(S{iS}.GFPabs(ind141:ind221)))*3/60:3/60:(221-1)*3/60,...
-                S{iS}.GFPabs(ind141:ind221)./10e6,'-','Color',c(3,:))
-            hold on
-        else
-            ind221 = 1;
-        end
-        
-        %induction 2
-        ind281 = find(S{iS}.DF:S{iS}.LF==281);
-        if isempty(ind281) == 0
-            plot((281-length(S{iS}.GFPabs(ind221:ind281)))*3/60:3/60:(281-1)*3/60,...
-                S{iS}.GFPabs(ind221:ind281)./10e6,'-','Color',c(4,:))
-            hold on
-        else
-            ind281 = 1;
-        end
-        
-        %repression 2
-        plot((320-length(S{iS}.GFPabs(ind281:end)))*3/60:3/60:(320-1)*3/60,...
-            S{iS}.GFPabs(ind281:end)./10e6,'-','Color',c(5,:))
-        hold on
-    end
-    
-end
-
-ylabel('total Gal1-GFP fluorescence (a.u.)')
-yticks([0,5,10,15])
-xlabel('experimental time (h)')
-xticks([0,4,7,11,14,16])
-box off
-set(gca,'linewidth',1.02)
-set(gca,'FontSize',11)
-set(gca,'FontName','Arial')
-ylim([0,15])
-xlim([0,16])
-set(gcf, 'DefaultFigureRenderer', 'painters');
-set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 5.5 5])
-print('-dpdf','./Figures/Fig2B','-painters')
-
-%% Figure 2D - non-dividing total GFP traces for repressions r1 and r2 for WT
+%% Figure 2B (left and right) - non-dividing total GFP traces for repressions r1 and r2 for WT
 
 clearvars;
 clc;
@@ -217,8 +114,8 @@ for irep = 1:2
     xlim([0,2])
     set(gcf, 'PaperUnits','centimeters', 'PaperPosition',[0 0 7 5])
     if irep == 1
-        print('-dpdf','./Figures/Fig2Dleft','-painters')
+        print('-dpdf','./Figures/Fig2Bleft','-painters')
     else
-        print('-dpdf','./Figures/Fig2Dright','-painters')
+        print('-dpdf','./Figures/Fig2Bright','-painters')
     end
 end
