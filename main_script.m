bc6e60521cd442c8e046d05cdc69dd78e7d3a3e1
leaschuh <lea.schuh@tum.de>
2022-10-11 14:47:02 +0200

    Update main_script.m

--- main_script.m
+++ main_script.m
@@ -182,6 +182,6 @@ comp_GFP0_tdelay(1,1,2,1,1)
 %repression 1 vs 2 for WT, replicate experiment
 comp_GFP0_tdelay(1,1,2,1,2)
 
-%% Check differetn degradation rates (simulation)
+%% Check different degradation rates (simulation)
 
 simDeg
